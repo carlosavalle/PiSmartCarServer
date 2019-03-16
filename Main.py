69cf11381d405c16d008b11d056f21e6f5efc2d2
carlosavalle <carlosavalle@users.noreply.github.com>
2019-03-15 23:02:29 -0600

    opencvobject

--- Main.py
+++ Main.py
@@ -1,16 +1,19 @@
 #from Socket2.SocketServer2  import *
 #from GPIO.Motors.Motor import *
 from Motor import *
+from OCVObject import *
 from SocketServer2 import *
 from time import sleep
 import socket
 import sys
+import threading
 
 #Socket Server
-Ser = SocketServer2("192.168.111.134",888)
+#Ser = SocketServer2("192.168.43.201",888)
+Ser = SocketServer2("192.168.111.10",888)
 #Ser = SocketServer2("172.0.0.1",888)
 Ss=Ser.Connect2()
-print ('Socket is now listening')
+
 
 #Start Motors
 
@@ -23,74 +26,126 @@ MotorB.standby(True)
 _direction = 0
 _forwardBackward = 0
 
+def forward(speed):
+    print("forward")
+    MotorA.drive(0)
+    MotorB.drive(speed)
+
+def forwardLeft(speed):
+    print("forwardLeft")
+    MotorA.drive(-100)
+    MotorB.drive(speed)
+
+def forwardRight(speed):
+    print("forwardRight")
+    MotorA.drive(100)
+    MotorB.drive(speed)
+
+def backward(speed):
+    print("backward")
+    MotorA.drive(0)
+    MotorB.drive(speed)
+
+def backwardRight(speed):
+    print("backwardRight")
+    MotorA.drive(-100)
+    MotorB.drive(speed)
+
+def backwardLeft(speed):
+    print("backwardLeft")
+    MotorA.drive(100)
+    MotorB.drive(speed)
+
+def ser():
+    print ('Socket is now listening')
+   # print(ObjectTrack.getPosition())
+    while 1:
+        conn, addr = Ss.accept()
+        # print ('Connect with ' + addr[0] + ':' + str(addr[1]))
+        buf = conn.recv(5)
+    
+        #print (buf.decode()) 
+        x = buf.decode()
+    
+        #middle
+        if (int(x[2:7]) >= -1 and int(x[2:7]) <= 1 and _direction !=0):
+            _direction = 0          
+            MotorA.drive(0)    
+        # Turn Right
+        if (int(x[2:7]) >= 2 and _direction !=2):
+          _direction = 2
+          MotorA.drive(100)    
+        #Turn Left
+        if (int(x[2:7]) <= -2 and _direction !=-2):
+           _direction = -2
+           MotorA.drive(-100)
+
+        #forward  
+    
+        if (int(x[0:2]) <= 2 and _forwardBackward !=2):
+           _forwardBackward = 2
+           MotorB.drive(100)
+
+        if (int(x[0:2]) == 3 and _forwardBackward !=3):
+           _forwardBackward = 3
+           MotorB.drive(80)
+
+        if (int(x[0:2]) == 4 and _forwardBackward !=4):
+           _forwardBackward = 4
+           MotorB.drive(60)
+    
+        if (int(x[0:2]) == 5 and _forwardBackward !=5):
+           _forwardBackward = 5
+           MotorB.drive(0)
+
+        if (int(x[0:2]) == 6 and _forwardBackward !=6):
+           _forwardBackward = 6
+           MotorB.drive(0)
+
+        # backward
+        if (int(x[0:2]) == 7 and _forwardBackward !=7):
+           _forwardBackward = 7
+           MotorB.drive(-60)
+
+        if (int(x[0:2]) == 8 and _forwardBackward !=8):
+           _forwardBackward = 8
+           MotorB.drive(-75)
+
+        if (int(x[0:2]) == 9 and _forwardBackward !=9):
+           _forwardBackward = 9
+           MotorB.drive(-90)
+
+        if (int(x[0:2]) == 10 and _forwardBackward !=10):
+           _forwardBackward = 10
+           MotorB.drive(-100)
+
+
+        #MotorB.drive(int(x[0:2]))
+
+       # print ("X " + x[0:2])    
+        #print ("Y " + x[2:7])    
+
+    s.close()
+
+
+
+
+
+
+
+
+ObjectTrack  = OCVObject()
+b = threading.Thread(name='ObjectTracker', target=ObjectTrack.detectObject)
+#ObjectTrack.detectObject()
+
+f = threading.Thread(name='SocketServer', target=ser)
+b.start()
+sleep(10)
+f.start()
+
 
 while 1:
-    conn, addr = Ss.accept()
-    # print ('Connect with ' + addr[0] + ':' + str(addr[1]))
-    buf = conn.recv(5)
-    
-    #print (buf.decode()) 
-    x = buf.decode()
-    
-    #middle
-    if (int(x[2:7]) >= -1 and int(x[2:7]) <= 1 and _direction !=0):
-        _direction = 0          
-        MotorA.drive(0)    
-    # Turn Right
-    if (int(x[2:7]) >= 2 and _direction !=2):
-      _direction = 2
-      MotorA.drive(100)    
-    #Turn Left
-    if (int(x[2:7]) <= -2 and _direction !=-2):
-       _direction = -2
-       MotorA.drive(-100)
-
-    #forward  
-    
-    if (int(x[0:2]) <= 2 and _forwardBackward !=2):
-       _forwardBackward = 2
-       MotorB.drive(100)
-
-    if (int(x[0:2]) == 3 and _forwardBackward !=3):
-       _forwardBackward = 3
-       MotorB.drive(80)
-
-    if (int(x[0:2]) == 4 and _forwardBackward !=4):
-       _forwardBackward = 4
-       MotorB.drive(60)
-    
-    if (int(x[0:2]) == 5 and _forwardBackward !=5):
-       _forwardBackward = 5
-       MotorB.drive(0)
-
-    if (int(x[0:2]) == 6 and _forwardBackward !=6):
-       _forwardBackward = 6
-       MotorB.drive(0)
-
-    # backward
-    if (int(x[0:2]) == 7 and _forwardBackward !=7):
-       _forwardBackward = 7
-       MotorB.drive(-60)
-
-    if (int(x[0:2]) == 8 and _forwardBackward !=8):
-       _forwardBackward = 8
-       MotorB.drive(-75)
-
-    if (int(x[0:2]) == 9 and _forwardBackward !=9):
-       _forwardBackward = 9
-       MotorB.drive(-90)
-
-    if (int(x[0:2]) == 10 and _forwardBackward !=10):
-       _forwardBackward = 10
-       MotorB.drive(-100)
-
-
-    #MotorB.drive(int(x[0:2]))
-
-   # print ("X " + x[0:2])    
-    #print ("Y " + x[2:7])    
-s.close()
-
+    print(ObjectTrack.getPosition())
 
 
 
---
+++ OCVObject.py
@@ -0,0 +1,115 @@
+# python dynamic_color_tracking.py --filter HSV --webcam
+
+import cv2
+import argparse
+import numpy as np
+import threading
+
+
+
+class OCVObject:
+    _position = 0
+    
+   # def __init__(self):
+       # self.main()
+
+    def callback(self, value):
+        pass
+
+
+    def setuptrackbars(self, range_filter):
+        cv2.namedWindow("Trackbars", 0)
+
+        for i in ["MIN", "MAX"]:
+            v = 0 if i == "MIN" else 255
+
+            for j in range_filter:
+                cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, self.callback)
+
+
+
+
+    def gettrackbarvalues(self, range_filter):
+        values = []
+
+        for i in ["MIN", "MAX"]:
+            for j in range_filter:
+                v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
+                values.append(v)
+        return values
+
+
+    def detectObject(self):
+        range_filter ='HSV'
+        camera = cv2.VideoCapture(0)
+        self.setuptrackbars(range_filter)
+        while True:
+
+            ret, image = camera.read()
+            #image = cv2.flip(image,1)
+
+            frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            #v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+            v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = self.gettrackbarvalues(range_filter)
+            #v2_min = 87
+            #lenovo cam
+            #v1_min = 22
+            #v2_min = 107
+            #v3_min = 95
+            #v1_max = 67
+            #v2_max = 255
+            #v3_max = 255
+
+            #logitec cam
+            v1_min = 24
+            v2_min = 34
+            v3_min = 42
+            v1_max = 60
+            v2_max = 115
+            v3_max = 248
+            
+            thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+
+            kernel = np.ones((5,5),np.uint8)
+            mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+            # find contours in the mask and initialize the current
+            # (x, y) center of the ball
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+            center = None
+ 
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                # find the largest contour in the mask, then use
+                # it to compute the minimum enclosing circle and
+                # centroid
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    #print(center[0])
+                    self._position = center[0]
+                   
+                    # draw the circle and centroid on the frame,
+                    # then update the list of tracked points
+                    cv2.circle(image, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(image, center, 3, (0, 0, 255), -1)
+                    cv2.putText(image,"centroid", (center[0]+10,center[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 255),1)
+                    cv2.putText(image,"("+str(center[0])+","+str(center[1])+")", (center[0]+10,center[1]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 255),1)
+                else:
+                    self._position = -1
+ 
+            # show the frame to our screen
+            cv2.imshow("Original", image)
+           # cv2.imshow("Thresh", thresh)
+            #cv2.imshow("Mask", mask)
+
+            if cv2.waitKey(1) & 0xFF is ord('q'):
+                break
+    def getPosition(self):
+        return self._position
